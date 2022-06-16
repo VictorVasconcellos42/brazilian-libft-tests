da8a0a8310193f247bdb394c203cd57d0fdc63df
vde-vasc <vde-vasc@student.42.rio>
2022-06-16 20:24:51 -0300

    header.h created

---
+++ include/header.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 07:42:15 by vde-vasc          #+#    #+#             */
+/*   Updated: 2022/06/16 20:23:55 by vde-vasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include "../../../../42Cursus/libft/libft.h"
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+void	test_ft_atoi(void);
+int		main_test(void);
+void	test_ft_bzero(void);
+void	test_ft_memcpy(void);
+#endif
