b89f313cb96d426d60e302ae4a63109f9dc7a1c5
vde-vasc <vde-vasc@student.42.rio>
2022-06-17 11:55:16 -0300

    Add new functions

--- include/header.h
+++ include/header.h
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 07:42:15 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/06/16 20:23:55 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/06/17 10:21:53 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@
 # include <string.h>
 # include <stdlib.h>
 
-void	test_ft_atoi(void);
 int		main_test(void);
+void	test_ft_atoi(void);
 void	test_ft_bzero(void);
 void	test_ft_memcpy(void);
+void	test_ft_memmove(void);
 #endif
